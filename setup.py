7a9656480e4c630e02849babd7fcc44edafbdef8
Qindan <qindan_zhu@berkeley.edu>
2020-05-31 18:39:55 -0700

    Add compilation on cython

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+exts = [Extension(name='pecans.chemderiv', sources=['pecans/chemderiv.pyx'])]
 
 setup(
     name="PECANS",
-    #ext_modules=cythonize('pecans/chemderiv.pyx')  # accepts a glob pattern
-    packages=['pecans']
+    ext_modules=cythonize(exts)  # accepts a glob pattern
+    #packages=['pecans']
 )
